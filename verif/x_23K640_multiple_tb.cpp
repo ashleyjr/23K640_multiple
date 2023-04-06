eb44267cfa510f002b10ca3cc7e6117c1e051c3d
Ashley J. Robinson <ashley181291@gmail.com>
2023-04-06 17:27:50 +0100

    basic working

--- verif/x_23K640_multiple_tb.cpp
+++ verif/x_23K640_multiple_tb.cpp
@@ -5,8 +5,9 @@
 #include <verilated_vcd_c.h>
 #include "Vx_23K640_multiple.h"
 #include <stdio.h>
-
-#define CYCLES 100000
+#include "../23K640/verif/include/app_driver.h"
+#include "../23K640/verif/include/sram_model.h"
+#include "../23K640/verif/include/logger.h"
 
 int main(int argc, char** argv, char** env) {
    
@@ -15,26 +16,128 @@ int main(int argc, char** argv, char** env) {
    Vx_23K640_multiple *dut = new Vx_23K640_multiple; 
    Verilated::traceEverOn(true);
    VerilatedVcdC *m_trace = new VerilatedVcdC; 
+      
+   uint32_t stop = std::stoi(argv[1]);
+   uint16_t addrs = std::stoi(argv[2]);
+   uint16_t backpressure = std::stoi(argv[3]);
+   uint16_t all_reads = std::stoi(argv[4]);
+   uint16_t all_writes = std::stoi(argv[5]);
+ 
+   Logger logger("logfile.txt", &sim_time);
+   AppDriver d(logger, addrs, backpressure, all_reads, all_writes);
    
+   SramModel m0(logger); 
+   SramModel m1(logger); 
+   SramModel m2(logger); 
+   SramModel m3(logger);   
+   SramModel m4(logger); 
+   SramModel m5(logger); 
+   SramModel m6(logger); 
+   SramModel m7(logger); 
+
    dut->trace(m_trace, 5);
    m_trace->open("waveform.vcd");
    
-   while (cycle < CYCLES) {
-              
+ 
+   dut->i_rst     = 1;
+   dut->i_valid   = 0;
+   dut->i_rd_n_wr = 0;
+   dut->i_addr    = 0;
+   dut->i_wdata   = 0;
+
+   while (cycle < stop) {
+      
+      // Falling Edge
+      dut->i_clk = 0;
+    
+      // Test Vectors
+      switch(cycle){   
+         // Reset
+         case 10:    dut->i_rst = 0;
+                     break;
+      }
+     
+      // AppDriver
+      d.set_inputs(
+         dut->o_accept,
+         dut->o_rdata,
+         dut->o_ready
+      );
+      d.advance();
+      dut->i_valid = d.get_valid();
+      dut->i_rd_n_wr = d.get_rd_n_wr();
+      dut->i_addr = d.get_addr(); 
+      dut->i_wdata = d.get_wdata();
+ 
+      // SRAM Models
+      m0.set_inputs(
+         dut->o_cs_0,
+         dut->o_so_0,      
+         dut->o_sck
+      );
+      dut->i_si_0 = m0.get_so(); 
+      m1.set_inputs(
+         dut->o_cs_1,
+         dut->o_so_1,      
+         dut->o_sck
+      );
+      dut->i_si_1 = m1.get_so(); 
+      m2.set_inputs(
+         dut->o_cs_2,
+         dut->o_so_2,      
+         dut->o_sck
+      );
+      dut->i_si_2 = m2.get_so(); 
+      m3.set_inputs(
+         dut->o_cs_3,
+         dut->o_so_3,      
+         dut->o_sck
+      );
+      dut->i_si_3 = m3.get_so(); 
+      m4.set_inputs(
+         dut->o_cs_4,
+         dut->o_so_4,      
+         dut->o_sck
+      );
+      dut->i_si_4 = m4.get_so(); 
+      m5.set_inputs(
+         dut->o_cs_5,
+         dut->o_so_5,      
+         dut->o_sck
+      );
+      dut->i_si_5 = m5.get_so(); 
+      m6.set_inputs(
+         dut->o_cs_6,
+         dut->o_so_6,      
+         dut->o_sck
+      );
+      dut->i_si_6 = m6.get_so(); 
+      m7.set_inputs(
+         dut->o_cs_7,
+         dut->o_so_7,      
+         dut->o_sck
+      );
+      dut->i_si_7 = m7.get_so(); 
+       
       // Tick
       dut->eval();
-      m_trace->dump(sim_time);
+      m_trace->dump(sim_time); 
       sim_time++;
-
+      
+      // Rising Edge
+      dut->i_clk = 1;
+      
       // Tick
       dut->eval();
       m_trace->dump(sim_time); 
       sim_time++;
 
-
-      cycle++;
+      // Event
+      cycle = sim_time >> 1; 
    }
    
+   d.report_bw();
+
    m_trace->close();
    delete dut;
    exit(EXIT_SUCCESS);
